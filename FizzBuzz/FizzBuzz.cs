cf675a77745b1ef56789c59cf99229e3f3360458
Baptiste LYET <baptiste.lyet@agixis.com>
2023-07-11 10:08:30 +0200

    first commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1
+{
+    public class FizzBuzz
+    {
+        [Test]
+        public void Should()
+        {
+            //Arrange act
+            Assert.AreEqual(5, 5);
+        }
+    }
+}
